52fc57c08b2049517877a52f8e86a0b94add186b
123 <897131608@qq.com>
2020-07-14 20:23:29 +0800

    用户

--- UserManagementSystem/src/dao/UserDao.java
+++ UserManagementSystem/src/dao/UserDao.java
@@ -82,9 +82,91 @@ public class UserDao {
         return 0;
     }
 
-    //3.删除用户
+    //3.删除用户信息
     public int delete(int id) {
-        
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "delete from usermessage where id=?";
+            connection = DBUtil.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,id);
+
+            int ret = ps.executeUpdate();
+            return ret;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, ps, rs);
+        }
+        //删除失败
+        return 0;
     }
+
+    //4.查找用户
+    public User find(int id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        User user = null;
+
+        String sql = "select * from usermessage where id=?";
+        try {
+            connection = DBUtil.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setName(rs.getString("name"));
+                user.setAddress(rs.getString("address"));
+
+                user.setAge(rs.getInt("age"));
+                user.setGender(rs.getString("gender"));
+                user.setQq(rs.getString("qq"));
+                user.setEmail(rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, ps, rs);
+        }
+        return user;
+    }
+
+    //5.更新用户信息,给一个用户对象
+    public int update(User updUser) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            String sql = "update usermessage set gender=?,age=?,address=?,qq=?,email=? where id=?";
+            connection = DBUtil.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,updUser.getGender());
+            ps.setInt(2,updUser.getAge());
+            ps.setString(3,updUser.getAddress());
+            ps.setString(4,updUser.getQq());
+            ps.setString(5,updUser.getEmail());
+            ps.setInt(6,updUser.getId());
+
+            int ret = ps.executeUpdate();
+            return ret;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, ps,null);
+        }
+        //更新失败
+        return 0;
+
+    }
+
+
 
 }
