ea416fddc91c9bc887199fbc7b2d5696e160c75f
123 <897131608@qq.com>
2020-07-20 20:22:23 +0800

    登陆servlet

--- UserManagementSystem/src/servlet/loginServlet.java
+++ UserManagementSystem/src/servlet/loginServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import com.sun.net.httpserver.HttpsServer;
+import entiy.User;
+import service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +12,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/loginServlet")
+
 public class loginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json;charset=utf-8");
+
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         System.out.println(username+password);
+
+        User loginUser =new User(); //创建一个数据库实体类对象
+        loginUser.setUsername(username);
+        loginUser.setPassword(password);
+
+        UserService userService=new UserService(); //创建service层的对象
+
+        User user = userService.login(loginUser);
+
+     
     }
 }
