244573548af7d0aa92d1b00951cc5dd5ab68c67f
123 <897131608@qq.com>
2020-07-24 23:03:57 +0800

    更新项目

---
+++ cash/src/test/java/servlet/GoodsPutAwayServletTest.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoodsPutAwayServletTest {
+
+    @Test
+    public void doPost() {
+        GoodsPutAwayServlet goodsPutAwayServlet = new GoodsPutAwayServlet();
+
+    }
+}
